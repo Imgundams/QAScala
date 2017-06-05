00041582fb248be2f053ff5149fec263deec5ad0
Administrator <imgundams@gmail.com>
2017-06-05 13:43:40 +0100

    InitialD

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -5,6 +5,14 @@ object helloWorld {
 
   def main(args: Array[String]) {
     println("Hello World! I work.")
+    val meaningOfLife = 42
+    println(s"The meaning of life is computed to be $meaningOfLife, Goodbye!")
+
+    var mutableVar = 10
+    mutableVar=mutableVar + 5
+    println(s"The mutable variable here is $mutableVar")
+
+
 
   }
 
