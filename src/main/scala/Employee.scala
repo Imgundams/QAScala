b936af8b1f3040861e32f4f465619fd6622a8c6e
Administrator <imgundams@gmail.com>
2017-06-06 15:11:22 +0100

    Tuesday's Work

---
+++ src/main/scala/Employee.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Administrator on 06/06/2017.
+  */
+
+ class Employee (fullName: String, contactNumber: Int,jobTitle:String, wage:Int) extends Person(fullName, contactNumber){
+    jobTitle:String
+    wage:Double
+
+
+}
--- src/main/scala/People.scala
+++ src/main/scala/People.scala
@@ -1,14 +1,9 @@
 /**
   * Created by Boss on 06/06/2017.
   */
-abstract class Person(var fullname:String, var contactNumber:Int, var age:Int ) {
+abstract class Person(fullName :String, contactNumber:Int) {
 
-  fullname: String
-  contactNumber: Int
-  age: Int
-def getName
+  def getfullName: String
 
-
-
-
+  def getContactNumber: Int
 }
