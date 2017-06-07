8f58836ff461e9cb0b73aaeb8c3509f60942075a
Imgundams <Imgundams@users.noreply.github.com>
2017-06-07 20:47:44 +0100

    Recent

---
+++ src/main/scala/RunnerClass.scala
@@ -0,0 +1,12 @@
+/*
+   Created by Boss on 06/06/2017.
+*/
+class RunnerClass{
+
+  def main(args: Array[String]): Unit = {
+    val garage = new Garage()
+
+    garage.addVehicle("Car", "Fiat", "panda", 6)
+  }
+
+}
